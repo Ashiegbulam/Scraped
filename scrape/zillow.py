a01d0c609e7e021c03513079b94b73a1965d54ff
Ashiegbulam <ashiegbulam@icloud.com>
2023-06-25 16:52:41 +0100

    new work

---
+++ scrape/zillow.py
@@ -0,0 +1,6 @@
+"""Script to get a list of houses in the jersey city of Nj with their prices
+and address from the zillow real estate website."""
+
+# sieve price from provided dropdown
+# collect required data
+# get link and loop through to collect multi pagenated data
